26b87ead6416ab509cbba2d1400e7476c7165016
M Rule <mrule7404@gmail.com>
2019-04-03 17:07:01 +0100

    large-file issue

--- sync.sh
+++ sync.sh
@@ -4,7 +4,30 @@ shopt -s extglob
 
 ./maketree.py
 
+find -type f -size +40M > ./.gitignore
+
+echo "THE FOLLOWING LARGE FILES ARE NOT TRACKED"
+cat ./.gitignore
+echo "THESE FILES ARE TOO LARGE AND ARE NOT TRACKED"
+
+#git add -A
+
+echo "Adding new files..."
 git add . 
-git add -u :/
+
+echo "Tracking updates..."
+git add -u
+
+# For some reason the above "git add" commands managed to ignore 
+# .gitignore (really!). 
+
+# hopefully removes everything that shouldn't be being tracked as per
+# .gitignore. Possibly dangerous. 
+cat .gitignore | awk "/^[.\*]/" | sed 's/"/"\\""/g;s/.*/"&"/' |  xargs -E '' -I{} git rm -rf --cached {}
+
+echo "Committing..."
+# :/
 git commit -m "$1"
+
+echo "Pushing to origin master..."
 git push origin master
